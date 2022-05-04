6167299db934ea91ba1e527c08e34891450ffd08
JinYiGao <497189242@qq.com>
2022-05-05 01:31:44 +0800

    'init'

---
+++ test.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: JinYiGao
+ * @Date: 2022-04-16 22:34:02
+ * @LastEditors: JinYiGao
+ * @LastEditTime: 2022-04-17 21:59:35
+ */
---
+++ test.py
@@ -0,0 +1,8 @@
+'''
+Descripttion: 
+version: 
+Author: JinYiGao
+Date: 2022-04-17 20:49:33
+LastEditors: JinYiGao
+LastEditTime: 2022-04-17 21:54:07
+'''
